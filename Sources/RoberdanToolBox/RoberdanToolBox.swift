172957a5aff010faba0aa7a44a6e944e088d509d
Roberdan <Roberdan@FightTheStroke.org>
2020-09-25 14:11:54 +0200

    initial empty package

--- Sources/RoberdanToolBox/RoberdanToolBox.swift
+++ Sources/RoberdanToolBox/RoberdanToolBox.swift
@@ -1,3 +1,12 @@
 struct RoberdanToolBox {
     var text = "Hello, World!"
 }
+
+struct SPMSwiftUITutorial {
+    var text = "Hello, World!"
+}
+
+
+public func myTestSPMFunction() {
+    print("SPM Test Function Succeeded!")
+}
