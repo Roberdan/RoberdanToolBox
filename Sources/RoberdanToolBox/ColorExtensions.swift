02319a7da3f2c911e05afd499a7f755f6e29507c
Roberdan <Roberdan@FightTheStroke.org>
2020-09-25 15:00:14 +0200

    color extensions

---
+++ Sources/RoberdanToolBox/ColorExtensions.swift
@@ -0,0 +1,211 @@
+//
+//  ColorExtensions.swift
+//  
+//
+//  Created by Roberto D’Angelo on 25/09/2020.
+//
+
+import Foundation
+import SwiftUI
+
+extension UIColor {
+    public var rgbComponents:(red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat) {
+        var r:CGFloat = 0
+        var g:CGFloat = 0
+        var b:CGFloat = 0
+        var a:CGFloat = 0
+        if getRed(&r, green: &g, blue: &b, alpha: &a) {
+            return (r,g,b,a)
+        }
+        return (0,0,0,0)
+    }
+    // hue, saturation, brightness and alpha components from UIColor**
+    public var hsbComponents:(hue: CGFloat, saturation: CGFloat, brightness: CGFloat, alpha: CGFloat) {
+        var hue:CGFloat = 0
+        var saturation:CGFloat = 0
+        var brightness:CGFloat = 0
+        var alpha:CGFloat = 0
+        if getHue(&hue, saturation: &saturation, brightness: &brightness, alpha: &alpha){
+            return (hue,saturation,brightness,alpha)
+        }
+        return (0,0,0,0)
+    }
+    
+    public var htmlRGBColor:String {
+        return String(format: "#%02x%02x%02x", Int(rgbComponents.red * 255), Int(rgbComponents.green * 255),Int(rgbComponents.blue * 255))
+    }
+    public var toJson:String {
+        return String(format: "#%02x%02x%02x", Int(rgbComponents.red * 255), Int(rgbComponents.green * 255),Int(rgbComponents.blue * 255))
+    }
+    public var htmlRGBaColor:String {
+        return String(format: "#%02x%02x%02x%02x", Int(rgbComponents.red * 255), Int(rgbComponents.green * 255),Int(rgbComponents.blue * 255),Int(rgbComponents.alpha * 255) )
+    }
+    //  sample use
+    //    let myColorBlack = UIColor.blackColor().toJson         //#000000ff
+    //    let myLghtGrayColor = UIColor.lightGrayColor().toJson  //#aaaaaaff
+    //    let myDarkGrayColor = UIColor.darkGrayColor().toJson
+}
+
+extension UIColor {
+    convenience init(hexString: String, alpha: CGFloat = 1.0) {
+        let hexString: String = hexString.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+        let scanner = Scanner(string: hexString)
+        // Tell scanner to skip the # character
+        scanner.charactersToBeSkipped = CharacterSet(charactersIn: "#")
+        
+        var color: UInt64 = 0
+        // Scan hex value
+        scanner.scanHexInt64(&color)
+        let mask = 0x000000FF
+        let r = Int(color >> 16) & mask
+        let g = Int(color >> 8) & mask
+        let b = Int(color) & mask
+        let red = CGFloat(r) / 255.0
+        let green = CGFloat(g) / 255.0
+        let blue = CGFloat(b) / 255.0
+        self.init(red:red, green:green, blue:blue, alpha:alpha)
+    }
+    
+    public func toHexString() -> String {
+        var r:CGFloat = 0
+        var g:CGFloat = 0
+        var b:CGFloat = 0
+        var a:CGFloat = 0
+        getRed(&r, green: &g, blue: &b, alpha: &a)
+        let rgb:Int = (Int)(r*255)<<16 | (Int)(g*255)<<8 | (Int)(b*255)<<0
+        return String(format:"#%06x", rgb)
+    }
+    
+    public func toUint() -> UInt {
+        var r:CGFloat = 0
+        var g:CGFloat = 0
+        var b:CGFloat = 0
+        var a:CGFloat = 0
+        getRed(&r, green: &g, blue: &b, alpha: &a)
+        let rgb:UInt = (UInt)(r*255)<<16 | (UInt)(g*255)<<8 | (UInt)(b*255)<<0
+        return rgb
+    }
+}
+
+extension UIColor {
+    public var color: Color {
+        get {
+            let rgbColours = self.cgColor.components
+            if rgbColours?.count == 2 {
+                return Color(
+                    red: Double(rgbColours![0]),
+                    green: Double(rgbColours![1]),
+                    blue : Double (0.0))
+            } else {
+                return Color(
+                    red: Double(rgbColours![0]),
+                    green: Double(rgbColours![1]),
+                    blue: Double(rgbColours![2]))
+            }
+        }
+    }
+    
+    public func rgb() -> Int? {
+        var fRed : CGFloat = 0
+        var fGreen : CGFloat = 0
+        var fBlue : CGFloat = 0
+        var fAlpha: CGFloat = 0
+        if self.getRed(&fRed, green: &fGreen, blue: &fBlue, alpha: &fAlpha) {
+            let iRed = Int(fRed * 255.0)
+            let iGreen = Int(fGreen * 255.0)
+            let iBlue = Int(fBlue * 255.0)
+            let iAlpha = Int(fAlpha * 255.0)
+            
+            //  (Bits 24-31 are alpha, 16-23 are red, 8-15 are green, 0-7 are blue).
+            let rgb = (iAlpha << 24) + (iRed << 16) + (iGreen << 8) + iBlue
+            return rgb
+        } else {
+            // Could not extract RGBA components:
+            return nil
+        }
+    }
+}
+
+extension Color {
+    public func uiColor() -> UIColor {
+        let components = self.components()
+        return UIColor(red: components.r, green: components.g, blue: components.b, alpha: components.a)
+    }
+    
+    private func components() -> (r: CGFloat, g: CGFloat, b: CGFloat, a: CGFloat) {
+        let scanner = Scanner(string: self.description.trimmingCharacters(in: CharacterSet.alphanumerics.inverted))
+        var hexNumber: UInt64 = 0
+        var r: CGFloat = 0.0, g: CGFloat = 0.0, b: CGFloat = 0.0, a: CGFloat = 0.0
+        
+        let result = scanner.scanHexInt64(&hexNumber)
+        if result {
+            r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
+            g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
+            b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
+            a = CGFloat(hexNumber & 0x000000ff) / 255
+        }
+        return (r, g, b, a)
+    }
+    
+    init(hex: String) {
+        let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var int: UInt64 = 0
+        Scanner(string: hex).scanHexInt64(&int)
+        let a, r, g, b: UInt64
+        switch hex.count {
+        case 3: // RGB (12-bit)
+            (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
+        case 6: // RGB (24-bit)
+            (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
+        case 8: // ARGB (32-bit)
+            (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
+        default:
+            (a, r, g, b) = (1, 1, 1, 0)
+        }
+        
+        self.init(
+            .sRGB,
+            red: Double(r) / 255,
+            green: Double(g) / 255,
+            blue:  Double(b) / 255,
+            opacity: Double(a) / 255
+        )
+    }
+}
+
+/// Allows you to use Swift encoders and decoders to process UIColor
+
+public extension UIColor {
+    func codable() -> CodableColor {
+        return CodableColor(color: self)
+    }
+}
+
+public struct CodableColor {
+    /// The color to be (en/de)coded
+    let color: UIColor
+}
+
+extension CodableColor: Encodable {
+    public func encode(to encoder: Encoder) throws {
+        let nsCoder = NSKeyedArchiver(requiringSecureCoding: true)
+        color.encode(with: nsCoder)
+        var container = encoder.unkeyedContainer()
+        try container.encode(nsCoder.encodedData)
+    }
+}
+
+extension CodableColor: Decodable {
+    public init(from decoder: Decoder) throws {
+        var container = try decoder.unkeyedContainer()
+        let decodedData = try container.decode(Data.self)
+        let nsCoder = try NSKeyedUnarchiver(forReadingFrom: decodedData)
+        guard let color = UIColor(coder: nsCoder) else {
+            struct UnexpectedlyFoundNilError: Error {}
+            throw UnexpectedlyFoundNilError()
+        }
+        
+        self.color = color
+    }
+}
+
