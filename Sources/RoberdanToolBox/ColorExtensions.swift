efeeaa2eebd0b42584f0c1e0fec00dac4c8aa768
Roberdan <Roberdan@FightTheStroke.org>
2020-09-28 14:28:28 +0200

    fixed color extensions

--- Sources/RoberdanToolBox/ColorExtensions.swift
+++ Sources/RoberdanToolBox/ColorExtensions.swift
@@ -47,7 +47,7 @@ extension UIColor {
 }
 
 extension UIColor {
-    convenience init(hexString: String, alpha: CGFloat = 1.0) {
+    convenience public init(hexString: String, alpha: CGFloat = 1.0) {
         let hexString: String = hexString.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
         let scanner = Scanner(string: hexString)
         // Tell scanner to skip the # character
