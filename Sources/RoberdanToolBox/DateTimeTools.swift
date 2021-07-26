bc71804425017a48608bfb1172fde4e11042d260
Roberdan <roberdan@fightthestroke.org>
2021-07-26 13:49:18 +0200

    added .year to date extension

--- Sources/RoberdanToolBox/DateTimeTools.swift
+++ Sources/RoberdanToolBox/DateTimeTools.swift
@@ -191,6 +191,11 @@ extension Date {
     public var month: Int {
         return Calendar.current.component(.month,  from: self)
     }
+    
+    public var year: Int {
+        return Calendar.current.component(.year, from: .self)
+    }
+    
     public var isLastDayOfMonth: Bool {
         return dayAfter.month != month
     }
